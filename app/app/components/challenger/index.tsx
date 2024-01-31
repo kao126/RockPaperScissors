6809d6deed8fe6a6e0c4b6cbe3a04f43448cf06d
oshima <oshima@fundely.co.jp>
2024-01-31 16:54:55 +0900

    delete: erase unnecessary docs

--- app/app/components/challenger/index.tsx
+++ app/app/components/challenger/index.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 
-import Rock from '../../../public/Rock.png'
-import Paper from '../../../public/Paper.png'
-import Scissors from '../../../public/Scissors.png'
-
 import RockB from '/public/Rock_B.png'
 import PaperB from '/public/Paper_B.png'
 import ScissorsB from '/public/Scissors_B.png'
 
 import Button from '../commons/button';
-
-
-// import type { BtnsProps } from '../../../types/button'
-
-// type Props = {
-//   buttons: BtnsProps[]
-// };
 
 const Challenger = () => {
   // コンポーネントの実装
--- app/app/components/dealer/index.tsx
+++ app/app/components/dealer/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Button from '../commons/button';
 
-// import type { BtnsProps } from '/types/button'
-
 import Rock from '/public/Rock.png'
 import Paper from '/public/Paper.png'
 import Scissors from '/public/Scissors.png'
@@ -12,9 +10,7 @@ import RockB from '/public/Rock_B.png'
 import PaperB from '/public/Paper_B.png'
 import ScissorsB from '/public/Scissors_B.png'
 
-// type props = {
-//   buttons: BtnsProps[]
-// };
+
 
 const Dealer = () => {
   return (
