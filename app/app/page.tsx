ad2555efa8aebd7063ecc773830baa7953c7634f
oshima <oshima@fundely.co.jp>
2024-01-31 15:45:23 +0900

    update: adjust layout on Safari

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -6,7 +6,7 @@ import Footer from './components/commons/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-screen max-w-screen-lg my-0 mx-auto text-center">
+    <div className="flex flex-col w-full h-screen max-w-screen-lg my-0 mx-auto text-center" style={{ maxHeight: "-webkit-fill-available"}}>
       <Header />
       <main className='h-full bg-slate-50'>
         <Dealer />
