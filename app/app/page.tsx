7f7f0ea933272c98139a3271916e12dc56f89370
oshima <oshima@fundely.co.jp>
2024-08-28 14:05:15 +0900

    style: change height 100vh to 100dvh

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -8,8 +8,8 @@ import Count from './components/commons/count';
 export default function Home() {
   return (
     <div
-      className="flex flex-col w-full h-screen max-w-screen-lg my-0 mx-auto text-center"
-      style={{ maxHeight: '-webkit-fill-available' }}
+      className="flex flex-col w-full h-dvh max-w-screen-lg my-0 mx-auto text-center"
+      // style={{ maxHeight: '-webkit-fill-available' }}
     >
       <Header />
       <main className="h-full bg-slate-50">
