de0403000b27e2fb1be34a632073987ae3f01d6e
oshima <oshima@fundely.co.jp>
2024-08-28 13:31:26 +0900

    style: make gap between btns

--- app/app/components/challenger/index.tsx
+++ app/app/components/challenger/index.tsx
@@ -9,7 +9,7 @@ import Button from '../commons/button';
 const Challenger = () => {
   // コンポーネントの実装
   return (
-    <div className="w-3/4 h-full flex justify-evenly my-0 mx-auto">
+    <div className="w-full h-full flex justify-center gap-4 my-0 mx-auto">
       <Button imageData={RockB} value="rock" alt="グー" disable={false} />
       <Button imageData={PaperB} value="paper"  alt="パー" disable={false} />
       <Button imageData={ScissorsB} value="scissors" alt="チョキ" disable={false} />
